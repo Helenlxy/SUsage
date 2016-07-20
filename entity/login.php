aed95c95592fc949e6c267c4f8e8748e4c094ca4
JerryLoveZhiXin <571339406@qq.com>
2016-07-20 21:35:24 +0800

    build 1D88: Use Ajax & Add a Cartoon
    build 1D88: Use Ajax & Add a Cartoon
    
    ~ 修改 | 使用Ajax方式验证登录
    + 新增 | 点击登录后不允许修改输入框内容
    + 新增 | 密码错误时弹出提示

--- entity/login.php
+++ entity/login.php
@@ -1,30 +1,3 @@
-<?php 
-session_start();
-require_once("../functions/to_sql.php");
-if(isset($_POST) && $_POST){
-  $StuID=$_POST['id'];
-  $query="SELECT * FROM sys_user WHERE stuid='{$StuID}'";
-  $rs=mysqli_fetch_array(mysqli_query($conn,$query));
-  $salt=$rs["salt"];
-  $pw=md5($_POST["pw"].$salt);
-  if($pw == $rs['pw']){
-    $_SESSION['nickname']=$StuID;
-    $_SESSION['headimg']=$rs['headimg'];
-    $_SESSION['userid']=$rs['uid'];
-    $_SESSION['SUmaster']=$rs['isMaster'];
-    $_SESSION['group']=$rs['depgroup'];
-    $_SESSION['SUname']=$rs['tname'];
-    header("Location: index.php");
-  }
-  
-  else{ 
-    
-  }
-  
-}
-?>
-
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -50,32 +23,60 @@ if(isset($_POST) && $_POST){
     </h2>
     <input class="text-input ipt" required="required" placeholder="SUsage ID" id="id">
     <input class="text-input ipt" type="password" required="required" placeholder="Password" id="pw">
-    <button onclick="tologin();" id="btn" class="btn raised green" style="width:60%;margin-top:40px">登陆</button>
+    <button onclick="toLogin();" id="btn" class="btn raised green" style="width:60%;margin-top:40px">登陆</button>
   </center>
 
   <!-- Footer -->
-  <center style="font-size:12px;color:#ffffff;padding-top:490px"><b>SUsage</b> 桌面版1.0 · <a onClick="displaytips(); return false" style="color:#FFFFFF">[点此测试密码提示]</a> · <a href="https://github.com/zhxsu/SUsage/wiki/%E5%B8%AE%E5%8A%A9%E4%B8%8E%E5%8F%8D%E9%A6%88%E4%B8%AD%E5%BF%83-%7C-Hints-&-Feedbacks" target="_blank" style="color:#ffffff">帮助与反馈中心 </a>·<a href="http://zhxsu.github.io/SUsage/" target="_blank" style="color:#ffffff"> 关于 | 开源许可及协议声明 </a>· ©2016 <a href="http://weibo.com/zxsu32nd" target="_blank" style="color:#ffffff;text-decoration:underline">执信学生会</a> <a href="http://weibo.com/zhxsupc" target="_blank"  style="color:#ffffff;text-decoration:underline">电脑部</a> · In tech we trust</center>
+  <center style="font-size:12px;color:#ffffff;padding-top:490px"><b>SUsage</b> 桌面版1.0 · <a onClick="showtips(); return false" style="color:#FFFFFF">[点此测试密码提示]</a> · <a href="https://github.com/zhxsu/SUsage/wiki/%E5%B8%AE%E5%8A%A9%E4%B8%8E%E5%8F%8D%E9%A6%88%E4%B8%AD%E5%BF%83-%7C-Hints-&-Feedbacks" target="_blank" style="color:#ffffff">帮助与反馈中心 </a>·<a href="http://zhxsu.github.io/SUsage/" target="_blank" style="color:#ffffff"> 关于 | 开源许可及协议声明 </a>· ©2016 <a href="http://weibo.com/zxsu32nd" target="_blank" style="color:#ffffff;text-decoration:underline">执信学生会</a> <a href="http://weibo.com/zhxsupc" target="_blank"  style="color:#ffffff;text-decoration:underline">电脑部</a> · In tech we trust</center>
 </article>
 </body>
 
 <script src="/SUsage/res/js/jquery-2.2.1.min.js" type="text/javascript"></script>
-<script src="/SUsage/res/js/md5.js" type="text/javascript"></script>
 
 <script type="text/javascript">
 var tips = document.getElementById('codetips');
-function displaytips(){ 
+
+function showtips(){ 
   tips.style.display = "block";
-  setTimeout("tips.style.display='none'",3000);
+  setTimeout("hidetips()",3000);
 }
-</script>
-<script>
-function tologin(){
+
+function hidetips(){
+  tips.style.display = "none";
+}
+
+function toIndex(){
+  window.location.href="index.php";
+}
+
+function ErrorPW(){
+  showtips();
+  $("#id")[0].disabled=0;
+  $("#pw")[0].disabled=0;
+  $("#btn")[0].disabled=0;
+  $("#btn").html("登录");
+}
+
+function toLogin(){
   $("#id")[0].disabled=1;
   $("#pw")[0].disabled=1;
   $("#btn")[0].disabled=1;
   $("#btn").html("验证中");
   var id=$("#id").val();
   var pw=$("#pw").val();
+  $.ajax({
+    type:"POST",
+    url:"verify.php",
+    data:{id:id,pw:pw},
+    success:function(g){
+      if(g=="1") toIndex();
+      else if(g=="2") ErrorPW();
+      else alert("未知错误！错误码："+g);
+    },
+    error:function(e){
+      alert("未知错误！错误码："+e);
+    }
+  });
 }
 </script>
 </html>
---
+++ entity/verify.php
@@ -0,0 +1,30 @@
+<?php 
+session_start();
+require_once("../functions/to_sql.php");
+if(isset($_POST) && $_POST){
+  $StuID=$_POST['id'];
+  $sql="SELECT * FROM sys_user WHERE stuid='{$StuID}'";
+  $query=mysqli_query($conn,$sql);
+  $rs=mysqli_fetch_array($query);
+  $salt=$rs["salt"];
+  $pw=md5($_POST["pw"].$salt);
+  if($pw == $rs['pw']){
+    $_SESSION['nickname']=$StuID;
+    $_SESSION['headimg']=$rs['headimg'];
+    $_SESSION['userid']=$rs['stuid'];
+    $_SESSION['SUmaster']=$rs['isMaster'];
+    $_SESSION['group']=$rs['depgroup'];
+    $_SESSION['SUname']=$rs['tname'];
+    die("1");
+  }else if($pw != $rs['pw']){
+    //密码不符
+    die("2");
+  }else{
+    //网络连接出错 | 未知错误
+    die("3");
+  }
+}else{
+  //没有POST数据
+  die("9");
+}
+?>
