b49f8c5bf910aaed6223d658d6ac86526806b4f4
SmallOyster <master@xshgzs.com>
2016-08-02 19:49:14 +0800

    build 2A27:Use JSON instead of TXT to save notice
    build 2A27:Use JSON instead of TXT to save notice
    
    ~ 修改 | 使用JSON代替TXT来储存最新的公告
    + 新增 | 公告发布人、发布时间

--- Admin/Task/toPubGlobalNotice.php
+++ Admin/Task/toPubGlobalNotice.php
@@ -1,10 +1,19 @@
 <?php
 require_once("../Includes/CheckLog.php");
-$GlobalNotice=@file_get_contents("../../GlobalNotice.txt");
+$all=file_get_contents("../../GlobalNotice.json");
+$all=json_decode($all);
+$Notice=urldecode($all->notice);
+$pubman=$all->pubman;
+$pubtime=$all->pubtime;
 
 if(isset($_POST) && $_POST){
   $content=$_POST['content'];
-  if(file_put_contents("../../GlobalNotice.txt",$content)>0){
+  $urlcode=urlencode($content);
+  $time=date("n月j日");
+  $man=$_SESSION['name'];
+  $input='{"pubman":"'.$man.'","pubtime":"'.$time.'","notice":"'.$urlcode.'"}';
+  
+  if(file_put_contents("../../GlobalNotice.json",$input) > 0){
     echo "<script>alert('发布成功！');history.go(-1);</script>";
   }else{
     echo "<script>alert('发布失败！请联系系统管理员！');history.go(-1);</script>";
@@ -17,7 +26,8 @@ if(isset($_POST) && $_POST){
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>SUsage 管理中心 :: 发布全局公告</title>
-  
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
   <!-- Bootstrap -->
   <link href="/SUsage/Admin/css/bootstrap.css" rel="stylesheet">
   <link href="https://cdn.bootcss.com/bootstrap/3.3.6/fonts/glyphicons-halflings-regular.svg" rel="stylesheet">
@@ -34,8 +44,8 @@ if(isset($_POST) && $_POST){
 <div class="row text-center" style="padding-top:40px"> 
 <div class="well col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 text-center col-xs-10 col-xs-offset-1">
   <h3 style="color:#4CAF50">发布全局公告</h3><hr>
-  当前公告：<?php echo $GlobalNotice; ?><br><hr>
-  <a style="position:absolute;top:13px;left:5%;cursor:pointer" onclick="history.back()">返回上一页</a>
+  <h4><font color="#FFA000"><b>当前公告</b></font></h4>发布人：<?php echo $pubman; ?><br>发布时间：<?php echo $pubtime; ?><br>公告内容：<?php echo $Notice; ?><br><hr>
+  <a style="position:absolute;top:20px;left:5%;cursor:pointer" onclick="history.back()"><img src="../img/back.png"></a>
   <div class="col-md-offset-2 col-md-8" style="line-height:12px;">
   
 <form method="post">
--- entity/index.php
+++ entity/index.php
@@ -8,11 +8,18 @@ $UID=$_SESSION['userid'];
 $group=$_SESSION['group'];
 $pubman=$_SESSION['truename'];
 $sql=mysqli_query($conn,"SELECT * FROM task_list WHERE redep LIKE '%$group%' OR pubman='$pubman' ORDER BY Taskid DESC");
+
+$all=file_get_contents("../GlobalNotice.json");
+$all=json_decode($all);
+$Notice=urldecode($all->notice);
+$pubman=$all->pubman;
+$pubtime=$all->pubtime;
 ?>
+
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<!--引入wangEditor.css等样式表-->
+		<!-- 引入CSS文件 -->
 		<link rel="stylesheet" href="../res/css/themes/Sinterface.css" />
 		<?php
 			if($_SESSION['SUmaster']==1){
@@ -24,13 +31,13 @@ $sql=mysqli_query($conn,"SELECT * FROM task_list WHERE redep LIKE '%$group%' OR 
 		<link rel="stylesheet" href="../res/css/editor/wangEditor.css">
 		<link rel="stylesheet" href="../res/css/modules/ex-united.css" />
 		<!--网站标题以及icon-->
-		<title>你的任务 / SUsage Tasklist </title>
+		<title>你的任务 / SUsage Tasklist</title>
 		<link rel="shortcut icon" href="../res/icons/title/task_128X128.ico"/>
 	</head>
 	<body style="position:absolute;width:80%;">
-		<!--导航栏从此开始 -->
+		<!-- [begin]导航栏 -->
 		<div class="ex-navbar-for-Desktop">
-			<!--用户标签-->
+			<!-- 用户标签 -->
 			<a href="UCenter.php" class="ex-dnavbar-userbox-descunderunfb" title="进入个人中心">
 			<div class="ex-dnavbar-userbox">
 				<div class="ex-dnavbar-userbox-avatarfixbox">
@@ -67,30 +74,38 @@ $sql=mysqli_query($conn,"SELECT * FROM task_list WHERE redep LIKE '%$group%' OR 
 				</a>
 			</div>
 		</div>
-		<!--导航栏结束 -->
-		<!--退出提示-->
+		<!-- [end]导航栏结束 -->
+		<!-- [begin]退出提示 -->
 		<div class="toast" id="toast-exit" style="position:fixed;width:100%;height:69px;z-index:100;display:none;">
 			<label class="toast-label" style="font-family:微软雅黑;color:#ffffff;position:absolute;left:10%;line-height:45px;">你你你你你你你~真的要退出吗w</label>
 			<button class="btn" style="font-family:微软雅黑;color:#ffffff;position:absolute;right:10%;line-height:55px;font-size:16px;cursor:pointer;" onclick="window.location.href='logout.php'">是的</button>
 			<button id="cancelexit" class="btn" style="font-family:微软雅黑;color:#ffffff;position:absolute;right:20%;line-height:55px;font-size:16px;font-weight:bold;cursor:pointer;">不是</button>
 		</div>
-		<!--全局通知-->
+		<!-- [end]退出提示 -->
+		<!-- [begin]全局通知 -->
 		<div id="globalnote" class="modhide">
 			<h3>最高指示</h3>
-			<p style="height:130px;width:80%;font-size:14px;position:relative;left:10%;overflow-y:auto;word-wrap: break-word;">
+			<p style="height:130px;width:80%;font-size:14px;position:relative;left:10%;overflow-y:auto;word-wrap:break-word;">
+			发布人：<?php echo $pubman; ?><br>
+			发布时间：<?php echo $pubtime; ?><br>
+			<span style="font-family: 'microsoft yahei'">——————————————————————</span><br>
 			<!--内容区域开始-->
-			以下是长段落显示测试的文本。<br><br>这个engineering drawing呢，我们就有几年用鸭嘴的笔，旁边一个小盒子。最痛苦的，就是鸭嘴笔把这个水弄到里面，描图的时候一下子就⋯然后就用刀片刮，这个就是描图是最痛苦的，而且这个效率efficiency⋯我的这个经历就是到了上海，到了89年的年初的时候，我在想我估计是快要离休了，我想我应该去当教授。于是我就给朱物华校长、张钟俊院长，给他们写了一个报告。他们说欢迎你来，不过，他这个Apply for Professor，那你要去做一个报告。我就做了一个能源与发展趋势的主要的节能措施，这个报告经过好几百个教授一致通过。那么上海交大教授当了以后我就做第二个报告，就是微电子工业的发展。这两个报告做了以后不久，过后，1989年的5月31号北京就把我调到北京去了。现在这个报告做了快20年了，所以呢我就去年呢在我们交大的学报，我发表了两篇文章，就是呼应这个89年的报告的。特别是昨天晚上，他又把我这个第二篇报告，还有我这十几年包括在电子工业部、上海市所做的有关于信息产业化的文章，总共我听他们讲是27篇。我也没有什么别的东西送给你们，我们拿来以后我叫钱秘书啊，就把这两个学报，两个学报的英文本，因为他们这里洋文好的人多得很呐，英文本，还有前面出过两本书，再加上昨天晚上出的这本书，送给郭伟华同志，给你送过来，那么给你们作为一个纪念。人呐就都不知道，自己就不可以预料。你一个人的命运啊，当然要靠自我奋斗，但是也要考虑到历史的行程，我绝对不知道，我作为一个上海市委书记怎么把我选到北京去了，所以邓小平同志跟我讲话，说“中央都决定啦，你来当总书记”，我说另请高明吧。我实在我也不是谦虚，我一个上海市委书记怎么到北京来了呢？但是呢，小平同志讲“大家已经研究决定了”，所以后来我就念了两首诗，叫“苟利国家生死以，岂因祸福避趋之”，那么所以我就到了北京。到了北京我干了这十几年我也没有什么别的，大概三件事：一个，确立了社会主义市场经济；第二个，把邓小平的理论列入了党章；第三个，就是我们知道的“三个代表”。如果说还有一点什么成绩就是军队一律不得经商！这个对军队的命运有很大的关系。因为我后来又干了一年零八个月，等于我在部队干了15年军委主席。还有九八年的抗洪也是很大的。但这些都是次要的，我主要的我就是三件事情，很惭愧，就做了一点微小的工作，谢谢大家。这是我用过的啊！？老由啊，现在他们文印的图章里面绝对没有这枚图章，都不知道有这么一个东西。这就是说明二院的档案工作做得太好了！你们给我搞的这本东西啊……Excited！
+      <?php echo $Notice; ?>
 			<!--内容区域结束-->
-			<br><br><span style="font-family: 'microsoft yahei'">————————以下空————————</span><!--此内容固定显示-->
+			<br><br>
+			<span style="font-family: 'microsoft yahei'">————————以下空————————</span>
 			</p>
 			<center><button class='btn fff' style="margin:10px 0 20px 0" onclick='closenote(); return false'>知道了啦</button></center>
 		</div>
+		<!--[end]全局通知-->
+
 		<div id="panel">
-		<!-- 放在顶上的链接-->
+		<!-- 放在顶上的版权声明-->
 		<div id="about" class="ex-about" style="position:absolute;top:90px;width:100%;text-align:center;z-index:1;">
-			<a href="" onclick="opennote() ;return false" style="background-color:#c90000;color: #fff;padding:1px 5px 1px 5px;border-radius:15px"><span>X月X日</span>最高指示</a>&#12288;
+			<a href="" onclick="opennote() ;return false" style="background-color:#c90000;color: #fff;padding:1px 5px 1px 5px;border-radius:15px"><span><?php echo $pubtime; ?></span>最高指示</a>&#12288;
 			<a href="ucenter.php#helper" target="_blank" style="color:#00C853">帮助与反馈中心 </a>·<a href="http://zhxsu.github.io/SUsage/" target="_blank" style="color:#00C853"> 关于 | 开源许可及协议声明 </a> <span class="trick" title="用鼠标刮这里看看">试试alt+shift+g</span> <a id="ver"></a> ©2016 <a href="http://weibo.com/zxsu32nd" target="_blank" style="color:#9e9e9e">执信学生会</a> <a href="http://weibo.com/zhxsupc" target="_blank"  style="color:#9e9e9e">电脑部</a> · In tech we trust 
 		</div>
+		
 		<!-- 发布器以及任务界面 -->
 		<div id='poster' class='card rich-card'>
 			<h3 style='font-family:微软雅黑;margin-top:5px;left:0px;font-size:16px;position:relative;margin-left:15px;line-height:20px;color:#bbb'>发布任务( · ω · )<span style="position:relative;color:#FF0000;margin-top:5px;font-family:微软雅黑;font-size:12px;text-align:center">&#12288;本页面已禁用F5键以防止误触导致草稿丢失【千万别以为键盘坏了x——夏酱</span></h3>
@@ -164,7 +179,7 @@ $sql=mysqli_query($conn,"SELECT * FROM task_list WHERE redep LIKE '%$group%' OR 
 			<button class='btn raised green' id='laststep' onclick='gotoLastStep(); return false' style='display:none'>上一步</button>
 			<button class='btn raised green' id='submit' style='display:none' onclick='GetTaskInfo();'>发布任务</button>
 		</div>
-		<!--任务界面-->
+		<!-- [begin]任务界面 -->
 		<p id="tips1">———— 你的任务 ————</p>
 		<div id="listarea">
 		<?php 
@@ -185,7 +200,6 @@ $sql=mysqli_query($conn,"SELECT * FROM task_list WHERE redep LIKE '%$group%' OR 
 				<div class="contentarea">
 				  <?php echo $rs['ct']; ?>
 				</div>
-				
 				<div class="card-footer">
 				<?php
 				if($name==$pubman){//自己发布的任务
@@ -196,7 +210,6 @@ $sql=mysqli_query($conn,"SELECT * FROM task_list WHERE redep LIKE '%$group%' OR 
 				<div id='click<?php echo $Tid;?>'><a class='del btn raised red' onclick='checkDel("<?php echo $Tid; ?>");'>删除此任务</a></div>
 				<div id='check<?php echo $Tid;?>' style='display:none'><a class='del btn raised redmore' onclick='DeleteTask("<?php echo $Tid; ?>");'>确认删除</a></div>
 				<a class='finishsum' href='' onclick='opencpt(); return false'><span class='sumsty'><?php echo $cpt; ?></span>人完成了你的任务</a>
-				
 				<?php 
 				}else{
 				  $cpt_sql="SELECT * FROM task_complete WHERE Taskid='$Tid' AND Userid='$UID'";
@@ -204,23 +217,21 @@ $sql=mysqli_query($conn,"SELECT * FROM task_list WHERE redep LIKE '%$group%' OR 
 				  $cpt_rs=mysqli_fetch_array($cpt_query);
 				  if($cpt_rs["isComplete"]=="0"){
 				 ?>
-				
 				<div id="cptClick<?php echo $Tid; ?>"><button class='btn raised mark green' onclick='checkcpt("<?php echo $Tid; ?>");'>完成任务</button></div>
 				<div id="cptCheck<?php echo $Tid; ?>" style="display:none;" onclick='CompleteTask("<?php echo $Tid; ?>");'><button class='btn raised mark greenmore'>确认完成</button></div>
-    <?php }else{ ?>
+        <?php }else{ ?>
     			<div style="color:#4fb4f7;float:right;margin:10px"><span style="background-color:#00c857;color: #fff;padding:1px 5px 0 5px;border-radius:15px">√</span> 你已经完成任务</div>
-    <?php } }?>
+        <?php } }?>
+        </div>
+      </div>
+      <?php } ?>
+      <center class="ex-end" style="left:12.8%">——————再怎么找都没有啦~——————</center>
+      </div>
     </div>
-  </div>
-  <?php } ?>
-  <center class="ex-end" style="left:12.8%">——————再怎么找都没有啦~——————</center>
-  </div>
- </div>
-		<!--任务完成模块-->
-		<div id="whofinished" class="modhide"><!--接口任务ID-->
+		<!-- [begin]任务完成模块 -->
+		<div id="whofinished" class="modhide">
 			<h3>共<span><?php echo $cpt;?></span>人完成了此任务</h3>
 			<div style="height:230px;width:80%;position:relative;left:10%;overflow-y:auto">
-				<!--循环获取完成者信息-->
 				<div class="card fnspeople">
 					<img class="fnsimg" src="../storage/avatar/avatar-5117.jpg"/>
 					<span class="fnsname" title="XXXXXXXXXXXXXXXXXX">XXXXXXXXXXXXXXXXXX</span>
@@ -288,7 +299,6 @@ $sql=mysqli_query($conn,"SELECT * FROM task_list WHERE redep LIKE '%$group%' OR 
 			</div>
 			<center><button class='btn fff' style="margin:10px 0 20px 0" onclick='closecpt(); return false'>好，可以，行</button></center>
 		</div>
-
 
 <!--脚本引用-->
 <script src="../res/js/jquery-2.2.1.min.js"></script>
@@ -478,13 +488,11 @@ $.ajax({
 }); 
 }
 
-//彩蛋--关于我们  
 function easteregg(){
 	if(event.altKey && event.shiftKey && event.keyCode == 71){
 		window.location.href="about.html";
   }
 }
-
 </script>
 
 <?php
@@ -495,6 +503,5 @@ if($_SESSION['SUmaster']==1){
 	echo '<script type="text/javascript">document.onkeydown = function(){easteregg();};</script>';
 }
 ?>
-
 </body>
 </html>
